d9f061274db418144218177b6dd4e716b8e26a30
RonKbS <stephen.bamwite@gmail.com>
2018-07-16 00:20:24 +0300

    add notification in accountFile

--- UI/min_functionality.js
+++ UI/min_functionality.js
@@ -1,5 +1,23 @@
 
 
+
+
+function unveil() {
+    document.getElementById('drop').classList.toggle("show");
+}
+window.onclick = function(event){
+    if (!event.target.matches("dropped")){
+        var dropdowns = this.document.getElementsByClassName("links");
+        for (var i = 0; i < dropdowns.length; i++){
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')){
+                    openDropDown.classList.remove('show');
+                }
+        }   
+     }
+}
+
+/*
 let c = document.getElementsByClassName('each');
 let d =document.createElement('input');
 d.setAttribute('type', 'checkbox');
@@ -7,3 +25,4 @@ d.setAttribute('type', 'checkbox');
 while (true){
     c.appendChild(d);
 }
+*/
