c813bd1847f8f6a30243793cf1071daeb6ce8693
RonKbS <stephen.bamwite@gmail.com>
2018-07-29 04:23:51 +0300

    Refactor add_entry for use in model file

---
+++ testing_database/add_entry_user.py
@@ -0,0 +1,14 @@
+import psycopg2
+from config import config
+
+def add_entry_user(User_or_entry):
+    """Add new User to User table"""
+    placeholders = ', '.join(['%s'] * len(User_or_entry))
+    columns = ', '.join(User_or_entry.keys())
+    parameters = config()
+    connection = psycopg2.connect(**parameters)
+    cur = connection.cursor()
+    sql = '''INSERT INTO entries ( %s ) VALUES ( %s )'''  % (columns, placeholders)
+    cur.execute (sql, list(User_or_entry.values()))
+    connection.commit()
+    cur.close()
