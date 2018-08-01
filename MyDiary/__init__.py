3997846299835392e44e1538dabf5af3ee1cf469
RonKbS <stephen.bamwite@gmail.com>
2018-08-01 19:07:53 +0300

    Set database connection in config file

--- MyDiary/__init__.py
+++ MyDiary/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from testing_database.create_tables import create_tables
+from config import Config
 import os
 
 app = Flask(__name__)
+app.config['TESTING'] = False
 # app.config['SECRET_KEY'] = "\xb78\xd1\xd6\xff\x94IF\xbao\x13\xa8\x11\x94\xe0\x8d\xf3aU\xf7\xceK<\x97<x97"
-create_tables('users' , 'entries')
+# create_tables('users' , 'entries')
+Config.db
 
 from mydiary.api import bp as bp_api
 app.register_blueprint(bp_api, url_prefix='/api/v1')
---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from testing_database.create_tables import create_tables
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config(object):
+    '''Generated from print(uuid.uuid4().hex)'''
+    db = os.environ.get('DATABASE_URL') or create_tables('users' , 'entries')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'try_and_guess'
