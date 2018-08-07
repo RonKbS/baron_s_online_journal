7e422313bd160eaf48fd4da5378ce7d6e12cd2b8
RonKbS <stephen.bamwite@gmail.com>
2018-08-07 23:24:48 +0300

    Test for correct_user sign up

--- app/tests/test_users.py
+++ app/tests/test_users.py
@@ -8,17 +8,20 @@ import unittest
 
 user = {'name': 'jon', 'email': 'ron@gmail.com', 'password': 'words'}
 user_sign_in = {'name': 'jon', 'password': 'words'}
+wrong_user = {'name': 'ron', 'password': 'words'}
+
 
 class TestUsers(unittest.TestCase):
     '''Run the following code before all tests'''
+
     def setUp(self):
         self.test_client = app.test_client()
         app.testing = True
 
     def tearDown(self):
         commands = (
-        "DELETE FROM entries",
-        "DELETE FROM users WHERE name='{}'".format('jon')
+            "DELETE FROM entries",
+            "DELETE FROM users WHERE name='{}'".format('jon')
         )
         ob = db()
         cur = ob.connection.cursor()
@@ -26,9 +29,25 @@ class TestUsers(unittest.TestCase):
             cur.execute(command)
         cur.close()
 
-    def test_signup(self):
-        request = self.test_client.post('http://127.0.0.1:5000/api/v1/auth/signup',
-                     data=json.dumps(user),
-                     content_type='application/json')
+    def test_correct_signup(self):
+        request = self.test_client.post(
+            'http://127.0.0.1:5000/api/v1/auth/signup',
+            data=json.dumps(user),
+            content_type='application/json')
         self.assertEqual(request.status_code, 201)
-        self.assertIn(json.loads(request.data.decode()), {'201': 'User added'})
+        reponse = json.loads(request.data.decode())
+        self.assertIn(reponse['Message'], 'User added')
+
+    def test_repeated_signup(self):
+        request1 = self.test_client.post(
+            'http://127.0.0.1:5000/api/v1/auth/signup',
+            data=json.dumps(user),
+            content_type='application/json')
+        request2 = self.test_client.post(
+            'http://127.0.0.1:5000/api/v1/auth/signup',
+            data=json.dumps(user),
+            content_type='application/json')
+        self.assertEqual(request1.status_code, 201)
+        self.assertEqual(request2.status_code, 400)
+        reponse = json.loads(request2.data.decode())
+        self.assertIn(reponse['Error'], 'User exists')
