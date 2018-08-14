c7dcc9d2399d845ce8eea21092385dc586f33666
RonKbS <stephen.bamwite@gmail.com>
2018-08-14 18:05:29 +0300

    Obtain formdata from using javascript

--- templates/app.js
+++ templates/app.js
@@ -1,14 +1,16 @@
+form = new FormData(document.getElementById('creds'))
+
 function signup() {
-    let creds = $('#creds').serializeArray().reduce(function (obj, item) {
-        obj[item.name] = item.value;
-        return obj;
-    }, {});
     fetch('http://127.0.0.1:5000/api/v1/auth/signup', {
             method: 'POST',
-            body: JSON.stringify(creds),
+            body: JSON.stringify({
+                'name': form.get('name'),
+                'email': form.get('mail'),
+                'password': form.get('lock')
+            }),
             headers: {
                 'Content-Type': 'application/json'
             }
         }).catch(error => console.error('Error:', error))
-        .then(response => console.log('Success:', response))
+        .then(response => console.log('Success:', response.json()))
 }
