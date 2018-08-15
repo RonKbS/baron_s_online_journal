955c20258881b81da21bfff057ae7d16cb477f85
RonKbS <stephen.bamwite@gmail.com>
2018-08-15 22:26:29 +0300

    Include cors option in js file for prompt response from server

--- app/templates/static/app.js
+++ app/templates/static/app.js
@@ -1,8 +1,8 @@
 
 function signup() {
-    // Let form = document.getElementById('creds');
     fetch('http://127.0.0.1:5000/api/v1/auth/signup', {
             method: 'POST',
+            mode: 'cors',
             body: JSON.stringify({
                 'name': document.getElementById('name').value,
                 'email': document.getElementById('mail').value,
