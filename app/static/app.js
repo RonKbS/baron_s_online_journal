65d7ce31a76ccc432123198dfe7845dfd36dfe58
RonKbS <stephen.bamwite@gmail.com>
2018-08-21 10:53:31 +0300

    Move app.js to static folder in app package

--- app/static/app.js
+++ app/static/app.js
@@ -1,18 +1,84 @@
-//https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
-
-let form = document.getElementById('creds');
 
 function signup() {
-    GlobalFetch.fetch('http://localhost/api/v1/auth/signup', {
+    fetch('http://127.0.0.1:5000/api/v1/auth/signup', {
             method: 'POST',
+            mode: 'cors',
             body: JSON.stringify({
-                'name': form.get('name'),
-                'email': form.get('mail'),
-                'password': form.get('lock')
+                'name': document.getElementById('name').value,
+                'email': document.getElementById('mail').value,
+                'password': document.getElementById('lock').value
             }),
             headers: {
                 'Content-Type': 'application/json'
             }
-        }).catch(error => console.error('Error:', error))
-        .then(response => console.log('Success:', response.json()))
+        }).then(Body => Body.json())
+        .then(data => {
+            if (data['Message'] === 'Karibu! Let\'s begin by logging in') {
+                alert(data['Message']);
+                window.location.href = 'index.html';
+            }
+            else if (data['Message'] != 'Karibu! Let\'s begin by logging in') {
+                alert(data['Message']);
+                return data['Message'];
+            }
+        })
+}
+function login() {
+    fetch('http://127.0.0.1:5000/api/v1/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'name': document.getElementById('name').value,
+                'password': document.getElementById('lock').value
+            }),
+            mode: 'cors',
+            redirect: 'manual'
+        })
+        .then(Response => Response.json())
+        .then(reply => {
+            if (!reply['Message']) {
+                let key = 'token';
+                let value = reply['token'];
+                localStorage.setItem(key, value);
+                console.log(localStorage.getItem('token'));
+                window.location.href = 'home_page.html';
+            } else if (reply['Message']) {
+                return alert(reply['Message'])
+            }
+            return true;
+        })
+        .catch(error => alert(error))
+}
+
+function logout() {
+    localStorage.removeItem('token');
+    window.location.href = 'index.html';
+}
+
+function add_entry() {
+    fetch('http://127.0.0.1:5000/api/v1/auth/entries', {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify({
+                'title': document.getElementsByClassName('titles').value,
+                'content': document.getElementsByClassName('adding').value
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                'token': localStorage.getItem('token')
+            }
+        }).then(Body => Body.json())
+        .then(data => {
+            if (data['Message'] === 'Entry added') {
+                alert('Thoughts immortalized');
+                window.location.href = 'home_page.html';
+            }
+            else if (data['Message'] != 'Entry added') {
+                alert(data['Message']);
+                return data['Message'];
+            }
+        })
+
 }
