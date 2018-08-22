ff3c49c2410fea3adeb3f1541476ad135d6cb368
RonKbS <stephen.bamwite@gmail.com>
2018-08-22 08:44:22 +0300

    Add function to display entries

--- app/static/app.js
+++ app/static/app.js
@@ -85,3 +85,42 @@ function add_entry() {
         .catch(error => console.error(error))
 
 }
+
+function display_entries () {
+    let t = document.getElementById('entries_table');
+    fetch('http://127.0.0.1:5000/api/v1/entries', {
+            method: 'GET',
+            credentials: 'include',
+            cache: 'reload',
+            headers: {
+                'Content-Type': 'application/json',
+                'token': localStorage.getItem('token')
+            }
+        }).then(Body => Body.json())
+        .then(data => {
+            if (data['Entries'] != 'No entries') {
+                let len = data['Entries'].length;
+                for (let x = 0; x < len; x++) {
+                    let r = t.insertRow(x);
+                    let c = r.insertCell();
+                    c.className = 'each';
+                    let box = document.createElement("input type='checkbox'");
+                    c.appendChild(box)
+                    let title = document.createElement("a onclick='view_entry()'");
+                    c.appendChild(title);
+                    
+                }
+                return window.location.href = 'home_page.html';
+            }
+            else if (data['Entries'] === 'No entries') {
+                let r = t.insertRow();
+                    let c = r.insertCell();
+                    c.className = 'each';
+                    let title = document.createElement("a onclick='view_entry()'");
+                    let text = document.createTextNode(data['Entries']);
+                    title.appendChild(text)
+                    c.appendChild(title);
+            }
+        })
+        .catch(error => console.error(error))
+}
